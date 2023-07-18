a1f4cb45abc110e250203aec5d06f1c8e0290790
saurabhlm10 <saurabhlm10@gmail.com>
2023-07-18 20:52:01 +0530

    changed client url to env

--- app.ts
+++ app.ts
@@ -17,7 +17,7 @@ connectToDb();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: process.env.CLIENT_URL,
     credentials: true,
   })
 );
--- dist/app.js
+++ dist/app.js
@@ -18,7 +18,7 @@ const auth_1 = require("./middleware/auth");
 const app = (0, express_1.default)();
 (0, db_1.default)();
 app.use((0, cors_1.default)({
-    origin: "http://localhost:3000",
+    origin: process.env.CLIENT_URL,
     credentials: true,
 }));
 app.use((0, morgan_1.default)("tiny"));
